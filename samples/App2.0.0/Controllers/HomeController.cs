0575f15b25f677c1a70483da4ebf69685a10e5c2
BrennanConroy <brecon@microsoft.com>
2017-07-18 15:06:27 -0700

    1.0.X 1.1.X and 2.0.0 applications

---
+++ samples/App2.0.0/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using App2._0._0.Models;
+
+namespace App2._0._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(typeof(object).GetTypeInfo().Assembly.Location);
+            sb.AppendLine(typeof(Controller).GetTypeInfo().Assembly.Location);
+            sb.AppendLine(typeof(IServiceCollection).GetTypeInfo().Assembly.Location);
+            ViewData["Message"] = sb.ToString();
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
